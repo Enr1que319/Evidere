d85b880dc521f6dced5f1614e435ddf6c6d06553
Enrique Vazquez <enrique_z500@hotmail.com>
2024-06-09 22:16:53 -0600

    Archivo que apunta al html de la generacion de los botones

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const quizButton = document.getElementById('quizButton');
+    quizButton.addEventListener('click', function() {
+        location.href = '/quiz.html';
+    });
+});
